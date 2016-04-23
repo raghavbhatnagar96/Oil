4996d2285830018a73851337360e69904f67dd56
Anshul-Gupta24 <science_maniac24@yahoo.in>
2016-04-24 02:41:34 +0530

    close!!!

--- parallel.cpp
+++ parallel.cpp
@@ -1,7 +1,10 @@
 int bottom(int ***areas,int N,int M,int n,int matx,int maty,int K)
 {
     if(intersect(0,n,M-1,N-1,matx,maty,K)==0)
+    {
+        //cout<<M<<" "<<(N-n)<<" "<<areas[M][N-n][2]<<endl;
         return areas[M][N-n][2];
+    }
     
     return 0;
 }
@@ -9,7 +12,10 @@ int bottom(int ***areas,int N,int M,int n,int matx,int maty,int K)
 int right(int ***areas,int N,int M,int m,int matx,int maty,int K)
 {
     if(intersect(m,0,M-1,N-1,matx,maty,K)==0)
+    {
+        //cout<<(M-m)<<" "<<N<<" "<<areas[M-m][N][1]<<endl;
         return areas[M-m][N][1];
-    
+    }
+
     return 0;
 }
