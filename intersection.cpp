772a16f67302c230c896d15c2f8147dab42ca206
Raghav Bhatnagar <raghavbhatnagar96@gmail.com>
2016-04-24 09:30:55 +0530

    Changes

--- intersection.cpp
+++ intersection.cpp
@@ -12,6 +12,16 @@ int isInside(int w, int x, int y, int z, int a, int b)
 
 int intersect(int w, int x, int y, int z, int a, int b, int k)//(w,x) and (y,z) are the diagonal points of area. (a,b) is the upper right point of the sub-matrix
 {
+    int areaL=y-w;
+    int areaB=z-x;
+    if((areaL<k)||(areaB<k))
+    {
+        return 1;
+    }
+    if((w==a)&(x==b)&(y==a+k-1)&(z=b+k-1))
+    {
+	    return 1;
+    }
     if(isInside(w, x, y, z, a, b))
     {
         return 1;
