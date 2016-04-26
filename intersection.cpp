42aa683336a974233c1f32cd1efc4a48cd4ddbb2
Raghav Bhatnagar <raghavbhatnagar96@gmail.com>
2016-04-27 00:59:34 +0530

    Reduced clutter

--- intersection.cpp
+++ intersection.cpp
@@ -13,18 +13,11 @@ int intersect(int w, int x, int y, int z, int a, int b, int k)//(w,x) and (y,z) 
 {
     int areaL=y-w+1;
     int areaB=z-x+1;
-    
+
     if((areaL<k) || (areaB<k))
     {
         return 1;
     }
-    
-    /*
-    if((w==a)&(x==b)&(y==a+k-1)&(z=b+k-1))
-    {
-	    return 1;
-    }
-    */
     else if(isInside(w, x, y, z, a, b))
     {
         return 1;
@@ -44,11 +37,3 @@ int intersect(int w, int x, int y, int z, int a, int b, int k)//(w,x) and (y,z) 
     else
         return 0;
 }
-
-/*
-int main()
-{
-    std::cout << doesIntersect(6,6,9,9,3,3,3);
-    return 0;
-}
-*/
